00cb22747d9291b729d1ee546b335d094437c074
Muhammad Syafiq Mazli <m.syfqmzli@gmail.com>
2026-02-27 00:47:03 +0800

    Initial terraform playbook

---
+++ terraform/main.tf
@@ -0,0 +1,13 @@
+provider "aws" {
+  region = var.aws_region
+}
+
+resource "aws_instance" "app_server" {
+  ami           = var.ami_id
+  instance_type = var.instance_type
+  key_name      = var.key_name
+
+  tags = {
+    Name = "jokes-app-server"
+  }
+}
